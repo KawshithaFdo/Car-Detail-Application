eb72ad36172b972b51c2bea98744096bcac894dc
KawshithaFdo <praveenkawshika456@gmail.com>
2022-10-02 14:50:13 +0530

    Content Update

--- Front-End/car_app/App.js
+++ Front-End/car_app/App.js
@@ -1,11 +1,24 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import Login from './Screens/Login'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack';
+import NewUser from './Screens/NewUser';
+
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-   
-     <Login/>
+   <NavigationContainer>
+      <Stack.Navigator>
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="NewUser" component={NewUser} />
+      {/* <Stack.Screen name="Profile" component={Profile} />
+      <Stack.Screen name="Settings" component={Settings} /> */}
+    </Stack.Navigator>
+   </NavigationContainer>
+     
     
   )
 }
--- Front-End/car_app/Screens/NewUser.js
+++ Front-End/car_app/Screens/NewUser.js
@@ -1,23 +1,20 @@
-import React from 'react'
 import { NativeBaseProvider,Text,Input,Button,Box } from 'native-base'
+import React from 'react'
 
-
-export default function Login({navigation}) {
-
+export default function NewUser() {
   return (
     <NativeBaseProvider>
-        <Text fontSize="3xl" bold underline mt="10%" ml="30%">User Login</Text>
+        <Text fontSize="3xl" bold underline mt="10%" ml="30%">New User</Text>
         <Box alignItems={"center"} padding={15}>
 
             <Input mx="4" placeholder="User Name" w="80%" borderRadius={100} marginTop={20} />
+            <Input mx="4" placeholder="Address" w="80%" borderRadius={100} marginTop={5} />
+            <Input mx="4" placeholder="Contact" w="80%" borderRadius={100} marginTop={5} />
             <Input type='password' mx="4" placeholder="Password"  w="80%" borderRadius={100} marginTop={5} />
             <Button size="lg" variant="solid" colorScheme="primary" borderRadius={100} marginTop={8} w={200} >
-                        Login
+                        Create account
             </Button>
         </Box>
-        <Button size="lg" variant="link" colorScheme="primary"  marginTop={8} w={200} onPress={()=>{navigation.navigate("NewUser")}} >
-                      Create an account  
-            </Button>
     </NativeBaseProvider>
   )
 }
