0cfea67c62c6da6c57c3abddf5d0fb6fea2360e4
KawshithaFdo <praveenkawshika456@gmail.com>
2022-10-02 14:08:11 +0530

    Implement Front End

---
+++ Front-End/car_app/App.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Login from './Screens/Login'
+
+export default function App() {
+  return (
+   
+     <Login/>
+    
+  )
+}
---
+++ Front-End/car_app/Screens/Login.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native'
+import React from 'react'
+import { NativeBaseProvider,Text,Input,Button,Box } from 'native-base'
+
+export default function Login() {
+
+  return (
+    <NativeBaseProvider>
+        <Text fontSize="3xl" bold underline mt="10%" ml="30%">User Login</Text>
+        <Box alignItems={"center"} padding={15}>
+
+            <Input mx="4" placeholder="User Name" w="80%" borderRadius={100} marginTop={20} />
+            <Input type='password' mx="4" placeholder="Password"  w="80%" borderRadius={100} marginTop={5} />
+            <Button size="lg" variant="solid" colorScheme="primary" borderRadius={100} marginTop={8} w={200} >
+                        Login
+            </Button>
+        </Box>
+        <Button size="lg" variant="link" colorScheme="primary"  marginTop={8} w={200} >
+                        Create an account
+            </Button>
+    </NativeBaseProvider>
+  )
+}
